097a4a4cf5096b7423ca99d7ee69431fb0c80319
Ahmed qureshi <rapshek@gmail.com>
2019-05-06 14:14:59 +0500

    first commit

---
+++ models/info.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+  var Schema = mongoose.Schema;
+
+  var InfoSchema = new Schema({
+    last_number:  Number,
+    po_num:Number,
+    invoice_num:Number,
+    sales_terms:String,
+    purchase_terms:String,
+    USD:Number,
+    GBP:Number,
+    EURO:Number,
+    AED:Number
+    
+  });
+
+  module.exports = mongoose.model('infos',InfoSchema);
